55eb0ce9562d458bbcf794f5b646f726d7268752
AStimer <AStimer@ADMINIB-N51C9VB>
2015-05-18 21:05:13 -0400

    - lots of things

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Game {
 
@@ -124,29 +126,215 @@ public class Game {
 		for (int i = 0; i < size; i++) {
 			
 			for (int j = 0; j < size; j++) { 
-				System.out.print(grid.get(i).get(j) + " "); 				
+				System.out.print(grid.get(i).get(j) + " "); 
+				
+				//System.out.print("(" + i + ","  + j + ") "); 
 			}
 			
 			System.out.println();
 		}
 	}
 	
-	public static void main(String[] args) { 
+	public static void main(String[] args) throws InterruptedException, IOException { 
 		Game game = new Game() ;
-		game.setSize(50);
+		game.setSize(10);
 		game.createGrid();
 		
-		game.setAlive(0);
-		;
-		game.setAlive(49);
 		
-		game.display();
+		int numGenerations = 10;
+		
+		Random randAlive = new Random();
+		
+		for (int x = 0; x < numGenerations; x++) { 	
+			for (int i = 0; i < 10; i++) { 
+				game.setAlive(randAlive.nextInt(99)); 
+			}
+			game.display();
+			
+			Thread.sleep(1000);
+			
+			Runtime.getRuntime().exec("cls.exe");
+		} 
 	}
 
 	public int getNumberAliveNeighbors(int id) {
 		
-		// TODO:
-		return 0;
+		int numNeighborsAlive = 0; 
 		
+		if (notOnBorder(id)) { 
+			if (isAlive(getLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopId(id))) numNeighborsAlive++; 
+			if (isAlive(getRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomRightId(id))) numNeighborsAlive++; 
+		}
+		else if (inTopLeft(id)) { 
+			if (isAlive(getRightId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomRightId(id))) numNeighborsAlive++; 
+		}
+		else if (inTopRight(id)) { 
+			if (isAlive(getLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomLeftId(id))) numNeighborsAlive++; 
+		}
+		else if (inBottomRight(id)) { 
+			if (isAlive(getTopId(id))) numNeighborsAlive++;
+			if (isAlive(getTopLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getLeftId(id))) numNeighborsAlive++; 
+		}
+		else if (inBottomLeft(id)) { 
+			if (isAlive(getTopId(id))) numNeighborsAlive++;
+			if (isAlive(getTopRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getRightId(id))) numNeighborsAlive++; 
+		}
+		else if (inLeftColumn(id)) {
+			if (isAlive(getTopId(id))) numNeighborsAlive++;
+			if (isAlive(getTopRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+		}
+		else if (inTopRow(id)) {
+			if (isAlive(getLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+			if (isAlive(getBottomRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getRightId(id))) numNeighborsAlive++; 
+		}
+		else if (inRightColumn(id)) {
+			if (isAlive(getLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getBottomId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopId(id))) numNeighborsAlive++; 
+		}
+		else if (inBottomRow(id)) {
+			if (isAlive(getLeftId(id))) numNeighborsAlive++;
+			if (isAlive(getTopRightId(id))) numNeighborsAlive++;
+			if (isAlive(getRightId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopLeftId(id))) numNeighborsAlive++; 
+			if (isAlive(getTopId(id))) numNeighborsAlive++; 
+		}
+		else {
+			throw new RuntimeException("Unexplained Error!!!");
+		};	
+		
+		return numNeighborsAlive;
+	}
+
+	private int getBottomRightId(int id) {
+		return id + size + 1;
+	}
+
+	private int getBottomLeftId(int id) {
+		return id + size - 1;
+	}
+
+	private int getTopRightId(int id) {
+		return id - size + 1;
+	}
+
+	private int getTopLeftId(int id) {
+		return id - size - 1;
+	}
+
+	private int getBottomId(int id) {
+		return id + size;
+	}
+
+	private int getRightId(int id) {
+		return id + 1;
+	}
+
+	private int getTopId(int id) {
+		return id - size;
+	}
+
+	private int getLeftId(int id) {
+		return id - 1;
+	}
+
+	public boolean inTopRow(int id) {
+		
+		if (id <= (size - 1)) { 
+			return true;
+		}
+		
+		return false;
+	}
+
+	public boolean inRightColumn(int id) {
+		
+		if ((id + 1) % size == 0) { 
+			return true;
+		}
+		
+		return false;
+	}
+
+	public boolean inBottomRow(int id) {
+		if (((Math.pow(size, 2) - size) <= id)  && (id <=  Math.pow(size, 2) - 1)) { 
+			return true;
+		}
+		return false;
+	}
+
+	public boolean inLeftColumn(int id) {
+		
+		if (id % size == 0) { 
+			return true;
+		}
+		
+		return false;
+	}
+
+	public boolean inTopRight(int id) {
+		if (inTopRow(id) && inRightColumn(id)) {
+			return true; 
+		}
+		return false;
+	}
+
+	public boolean inTopLeft(int id) {
+		if (inTopRow(id) && inLeftColumn(id)) {
+			return true; 
+		}
+		return false;
+	}
+
+	public boolean inBottomRight(int id) {
+		if (inBottomRow(id) && inRightColumn(id)) {
+			return true; 
+		}
+		return false;
+	}
+
+	public boolean inBottomLeft(int id) {
+		if (inBottomRow(id) && inLeftColumn(id)) {
+			return true; 
+		}
+		return false;
+	}
+
+	public boolean notOnBorder(int id) {
+		if (inTopRow(id)) {
+			return false;
+		}		
+		else if (inBottomRow(id)) {
+			return false;
+		}
+		else if (inLeftColumn(id)) {
+			return false;
+		}
+		else if (inRightColumn(id)) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 }
--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -74,16 +74,103 @@ public class TestGame {
 		game.setAlive(2);
 		assertTrue(game.isAlive(2));
 	}	
-	/*
+	@Test 
+	public void verifyTopRow() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inTopRow(2));
+	}
+	@Test 
+	public void verifyRightColumn() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inRightColumn(5));
+	}
+	@Test 
+	public void verifyBottomRow() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inBottomRow(7));
+	}
+	@Test 
+	public void verifyLeftColumn() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inLeftColumn(3));
+	}
+	@Test
+	public void verifyNotBorder() {
+		
+		game.setSize(4);
+		game.createGrid();
+		
+		assertTrue(game.notOnBorder(5));
+		assertTrue(game.notOnBorder(6));
+		assertTrue(game.notOnBorder(9));
+		assertTrue(game.notOnBorder(10));
+	}
+	
+	// checking the corner conditions now
+	
+	@Test 
+	public void verifyTopLeftCorner() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inTopLeft(0));
+	}
+
+	@Test 
+	public void verifyTopRightCorner() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inTopRight(2));
+	}
+	@Test 
+	public void verifyBottomLeftCorner() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inBottomLeft(6));
+	}
+	@Test 
+	public void verifyBottomRightCorner() {
+		
+		game.setSize(3);
+		game.createGrid();
+		
+		assertTrue(game.inBottomRight(8));
+	}
+
 	@Test
 	public void verifyThatICanGetNumberOfAliveNeighbors() { 
 		game.setSize(3);
 		game.createGrid();
 		
-		game.setAlive(2);
-		game.setAlive(0);
-		
-		assertEquals(2, game.getNumberAliveNeighbors(1));
-	}
-	*/
+		game.setAlive(1);
+		game.setAlive(5);
+		game.setAlive(7);
+	
+		assertEquals(1, game.getNumberAliveNeighbors(0));
+		assertEquals(1, game.getNumberAliveNeighbors(1));
+		assertEquals(2, game.getNumberAliveNeighbors(2));
+		assertEquals(2, game.getNumberAliveNeighbors(3));
+		assertEquals(3, game.getNumberAliveNeighbors(4));
+		assertEquals(2, game.getNumberAliveNeighbors(5));
+		assertEquals(1, game.getNumberAliveNeighbors(6));
+		assertEquals(1, game.getNumberAliveNeighbors(7));
+		assertEquals(2, game.getNumberAliveNeighbors(8));
+	}	
 }
